a6b180a8b189d615e65c57d53da58f6804b3f9ed
wchang101 <wonseok.chang@pwc.com>
2026-03-18 17:29:54 +0900

    chore: 동아리 홈페이지 프로젝트 개발 환경 세팅

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DDD - IT 사이드 프로젝트 동아리",
+  description: "개발자, 디자이너가 함께하는 IT 사이드 프로젝트 동아리 DDD입니다.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
