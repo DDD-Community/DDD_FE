75d2f9014f61c584964c6105893655bff1193d5b
Wonseok Chang <developerseok@gmail.com>
2026-03-24 21:52:38 +0900

    chore: 리액트 버전 업데이트 및 emotion 설치

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@ddd/ui"],
+  compiler: {
+    emotion: true,
+  },
 };
 
 export default nextConfig;
